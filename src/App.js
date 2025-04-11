ae8339c411ddefd8848f9db3baf7dabd5566b122
vinimaffra03 <viniciushmdem15@gmail.com>
2025-04-10 21:47:21 -0300

    logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./imgs/logo.svg"; // main logo from  alura books
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+          <p>
+            <strong> Mafra</strong>books
+          </p>
+        </div>
       </header>
     </div>
   );
